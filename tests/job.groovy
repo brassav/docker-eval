03e5084f0689be706dd7cec09a96415fd1b392f6
valentin brassart <valentin.brassart@epitech.eu>
2019-03-21 10:46:58 +0100

    groovy

--- tests/job.groovy
+++ tests/job.groovy
@@ -1,6 +1,6 @@
 job('job-v1') {
     scm {
-        git('https://github.com/brassav/docker-eval')
+        git('https://github.com/brassav/docker-eval', "v1")
     }
     triggers {
         scm('* H/1 * * *')
