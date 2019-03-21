47045ce5d69dc5fae688e5789ccefaf9c56cb84e
valentin <valentin.brassart@epitech.eu>
2019-03-21 10:59:21 +0100

    suppr site + git groovy

--- tests/job.groovy
+++ tests/job.groovy
@@ -2,7 +2,7 @@ job('job-v1') {
     scm {
         git('https://github.com/brassav/docker-eval', "v1") { node ->
             node / gitConfigName('valentin')
-            not / gitConfigEmail('valentin.brassart@estiam.com')
+            node / gitConfigEmail('valentin.brassart@estiam.com')
         }
     }
     triggers {
