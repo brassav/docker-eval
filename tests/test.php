296af556fb59b51465544f9e0751f573bbb62e78
valentin <valentin.brassart@epitech.eu>
2019-03-21 14:49:45 +0100

    sans les 2 bis

--- tests/test.php
+++ tests/test.php
@@ -3,7 +3,6 @@
 use PHPUnit\Framework\TestCase;
 
 require "samplephpwebsite/*";
-
 require "samplephpwebsite/functions.php";
 require "samplephpwebsite/config.php";
 
@@ -46,31 +45,6 @@ class Test extends TestCase
                 $expected =  ucwords(str_replace('/', '', str_replace('-', ' ', $page)));
                 
                 $actual = pageTitle();
-                $this->expectOutputString($expected);
-        }
-
-        public function testPageContent()
-        {
-                $page = isset($_GET['page']) ? $_GET['page'] : 'home';
-
-                if ($page == '/') $page = 'home';
-
-                $path = getcwd() . '/' . config('content_path') . '/' . $page . '.php';
-
-                if (file_exists(filter_var($path, FILTER_SANITIZE_URL))) {
-                       $expected =  include $path;
-                } else {
-                       $expected =  include config('content_path') . '/404.php';
-                }
-                $actual = pageContent();
-                $this->expectOutputString($expected);
-        }
-
-        public function testRun()
-        {
-                $expected = include config('template_path').'/templat e .php';
-
-                $actual = pageContent();
                 $this->expectOutputString($expected);
         }
 }
