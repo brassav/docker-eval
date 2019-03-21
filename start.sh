390077e6be9764ba66ed1f55a308489c62ea7cae
valentin <valentin.brassart@epitech.eu>
2019-03-21 15:13:27 +0100

    add script

---
+++ start.sh
@@ -0,0 +1,11 @@
+git checkout v1
+git pull
+
+##recuperation du sample v1
+rm -rf samplephpwebsite
+
+git clone https://github.com/Akasam/samplephpwebsite
+git checkout v1
+git pull
+
+docker-compose up
